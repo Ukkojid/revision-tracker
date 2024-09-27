19396970612bc3d75e009dcd9edc663d2e65e192
ukkojid <ukkojid@gmail.com>
2024-09-27 19:33:59 +0530

    Add frontend folder with React application

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Revision Tracker</h1>
+      <p>Your content goes here.</p>
+    </div>
+  );
+}
+
+export default App;
